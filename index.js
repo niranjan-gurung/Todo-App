e3a58892b2161d6c701c9fdac5b09cafdabb426b
niranjan-gurung <gurung5130@gmail.com>
2023-08-01 20:12:38 +0100

    new task has completed checkmark option + delete option (no functionality yet...)

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ btn.addEventListener('click', (e) => {
   
   // new list element:
   const node = document.createElement("li");
+  const input = document.createElement("input");
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('name', 'completed');
 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.append('delete');
+  
   // get input element:
   let task = document.getElementById('task');
-
+  
   if (task.value === "") {
     console.error('cannot submit empty task...');
     return;
   }
-  // append input's text value to new 'li' element:
-  node.append(task.value);
+
+  // add to new 'li' element:
+  node.append(task.value);      // add input's text value
+  node.appendChild(input);      // add completed checkbox
+  node.appendChild(deleteBtn);  // add delete button
 
   // Append 'li' to task list:
   document.getElementById("task-list").appendChild(node);
