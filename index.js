37f3cf17b3156639c22b9bd34da0eb1cbd178a0e
niranjan-gurung <gurung5130@gmail.com>
2023-08-02 12:30:19 +0100

    unique id for each list item

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('submit');
+const taskList = document.getElementById("task-list");
 
 btn.addEventListener('click', (e) => {
   // prevent page reload on button click:
@@ -6,11 +7,14 @@ btn.addEventListener('click', (e) => {
   
   // new list element:
   const node = document.createElement("li");
+  node.setAttribute('id', uniqueID());
+
   const input = document.createElement("input");
   input.setAttribute('type', 'checkbox');
   input.setAttribute('name', 'completed');
 
   const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute('id', 'deleteBtn');
   deleteBtn.append('delete');
   
   // get input element:
@@ -27,8 +31,18 @@ btn.addEventListener('click', (e) => {
   node.appendChild(deleteBtn);  // add delete button
 
   // Append 'li' to task list:
-  document.getElementById("task-list").appendChild(node);
+  taskList.appendChild(node);
 
   // reset input text
   task.value = "";
 });
+
+for (let i = 0; i < taskList.childNodes.length; i++) {
+  console.log(taskList.childNodes);  
+}
+
+// helper functions:
+// generate unique id for each list item:
+function uniqueID() {
+  return Math.floor(Math.random() * Date.now());
+}
